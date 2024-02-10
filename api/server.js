b0935a83a46c7af616d32aebe4b0fd8f36db5aee
jovan <jovancraft@gmail.com>
2024-02-10 17:34:20 -0600

    created my server file and index file

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require('express')
+const helmet = require('helmet')
+
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+    });
+  });
+
+  module.exports = server;
