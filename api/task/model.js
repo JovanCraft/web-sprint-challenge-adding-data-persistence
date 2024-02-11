25ba8b84baf47fd9265b9fe060770850d0592c36
jovan <jovancraft@gmail.com>
2024-02-11 17:59:03 -0600

    configured my findAllTasks and findTasksById functions and get endpoints

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,30 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+module.exports = {
+    findAllTasks,
+    findTasksById
+}
+
+async function findAllTasks(){
+    const rows = await db('tasks').join('projects').select('tasks.*', 'projects.project_name', 'projects.project_description')
+
+    return rows
+}
+
+async function findTasksById(task_id){
+    const rows = await db('tasks as t').join('projects as p', 't.project_id', 'p.project_id').select('t.*', 'p.project_name', 'p.project_description')
+    .where('t.task_id', '=', task_id)
+    return rows
+}
+
+
+
+/*
+    select
+    tasks.*,
+    projects.project_name,
+    projects.project_description
+from tasks
+join projects
+*/
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,25 @@
 // build your `/api/tasks` router here
+const express = require('express')
+const Task = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Task.findAllTasks()
+    .then(tasks => {
+        res.json(tasks)
+    })
+    .catch(next)
+})
+
+router.get('/:task_id', (req, res, next) => {
+    Task.findTasksById(req.params.task_id)
+    .then(task => {
+        res.json(task)
+    }).
+    catch(next)
+})
+
+
+
+module.exports = router
