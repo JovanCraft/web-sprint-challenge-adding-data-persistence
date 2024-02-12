ef8182df54c44dd903e70364d895aba8370baa1f
jovan <jovancraft@gmail.com>
2024-02-11 19:00:17 -0600

    passing 17/1 more to go/ got my task to show the correct things

--- api/task/model.js
+++ api/task/model.js
@@ -8,9 +8,19 @@ module.exports = {
 }
 
 async function findAllTasks(){
-    const rows = await db('tasks').join('projects').select('tasks.*', 'projects.project_name', 'projects.project_description')
+    const rows = await db('tasks as t')
+    .join('projects as p', 't.project_id', 'p.project_id')
+    .select('t.task_id',
+    't.task_description',
+    't.task_notes',
+    db.raw('(t.task_completed = 1) AS task_completed'),
+    'p.project_name',
+    'p.project_description')
 
-    return rows
+    return rows.map(task => ({
+        ...task,
+        task_completed: Boolean(task.task_completed)
+    }))
 }
 
 async function findTasksById(task_id){
